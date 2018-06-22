8c8b6eeda08bad85c74b2aca6034f24e798aac50
Kenneth R Sharp <ksharp@stanford.edu>
2018-06-21 20:18:29 -0700

    EARTH-904 More code cleanup.

--- src/Controller/StanfordEarthSamlController.php
+++ src/Controller/StanfordEarthSamlController.php
@@ -4,7 +4,6 @@ namespace Drupal\stanford_earth_saml\Controller;
 
 use Drupal\Core\Url;
 use Drupal\Core\Config;
-use Drupal\system\Controller;
 use Drupal\Core\Controller\ControllerBase;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
@@ -18,24 +17,25 @@ class StanfordEarthSamlController extends ControllerBase {
    */
   public function content() {
 
-    // make sure Varnish doesn't cache the redirect.
+    // Make sure Varnish doesn't cache the redirect.
     \Drupal::service('page_cache_kill_switch')->trigger();
-    // see if autologin is enabled.
+    // See if autologin is enabled.
     $auto403 = boolval(\Drupal::config('stanford_earth_saml.adminsettings')->get('stanford_earth_saml_auto403login'));
-    // only redirect to saml login if autologin is enabled and the user is anonymous.
+    // Only redirect to saml if autologin enabled and the user anonymous.
     if (\Drupal::currentUser()->isAnonymous() && $auto403) {
-      // set a ReturnTo parameter to the URI of the request that generated 403
+      // Set a ReturnTo parameter to the URI of the request that generated 403.
       $redirect = '/saml_login?ReturnTo=' .
-        Url::fromUserInput(\Drupal::request()->server->get('REQUEST_URI'), array('absolute' => TRUE))
-          ->toString();
-      // redirect the browser to Weblogin
+        Url::fromUserInput(\Drupal::request()->server->get('REQUEST_URI'), ['absolute' => TRUE])->toString();
+      // Redirect the browser to Weblogin.
       $response = new RedirectResponse($redirect, RedirectResponse::HTTP_FOUND);
       $response->send();
       return $response;
-    } else {
-      // no auto login or the user is already authenticated, so just do Drupal default
-      $drupalController = new Controller\Http4xxController();
+    }
+    else {
+      // No autologin or user is already authenticated so do Drupal default.
+      $drupalController = new Http4xxController();
       return $drupalController->on403();
     }
   }
+
 }
--- src/Controller/StanfordEarthSamlLogout.php
+++ src/Controller/StanfordEarthSamlLogout.php
@@ -1,17 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: kennethrsharp
- * Date: 6/21/18
- * Time: 4:31 PM
- */
 
 namespace Drupal\stanford_earth_saml\Controller;
 
-use Drupal\Core\Config;
-use Drupal\system\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 
+/**
+ * Class StanfordEarthSamlLogout.
+ *
+ * @package Drupal\stanford_earth_saml\Controller
+ */
 class StanfordEarthSamlLogout {
 
   /**
@@ -19,11 +16,11 @@ class StanfordEarthSamlLogout {
    */
   public function content() {
 
-    // make sure Varnish doesn't cache the redirect.
+    // Make sure Varnish doesn't cache the redirect.
     \Drupal::service('page_cache_kill_switch')->trigger();
-    // get the HttpReferer from the request
+    // Get the HttpReferer from the request.
     $redirect = \Drupal::request()->headers->get('referer', '/');
-    // redirect the browser to Weblogin
+    // Redirect the browser to Weblogin.
     $response = new RedirectResponse($redirect, RedirectResponse::HTTP_FOUND);
     $response->send();
     return $response;
